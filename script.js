f12d073cd50c96240cedb00f1d3db8b50881fb72
Christopher Aguiar <chrisaguiar72@gmail.com>
2021-06-01 18:16:06 -0400

    First commit

--- script.js
+++ script.js
@@ -4,75 +4,125 @@ function getRandomInt(max) {
 
 // FUNCTION TO GENERATE 3x8 !! FUNCTION TO GENERATE 3x8 !! FUNCTION TO GENERATE 3x8 !! FUNCTION TO GENERATE 3x8 !! FUNCTION TO GENERATE 3x8 !! 
 function generate_table3x8() {
-    // get the reference for the body
-    let body = document.getElementsByTagName("body")[0];
-  
-    // creates a <table> element and a <tbody> element
-    let tbl = document.createElement("table");
-    let tblBody = document.createElement("tbody");
-  
-    // creating all cells
-    for (let i = 0; i < 8; i++) {
-      // creates a table row
-      let row = document.createElement("tr");
-  
-      for (let j = 0; j < 3; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        let cell = document.createElement("td");
-        let cellText = document.createTextNode(getRandomInt(13) + " x " + getRandomInt(13) + " =    ");
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-  
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-    }
-  
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tbl);
+  if ($('#bigTable').length > 0) {
+    let elementTwo = document.getElementById('bigTable');
+    elementTwo.parentNode.removeChild(elementTwo);
 
   }
+  // get the reference for the body
+  let body = document.getElementsByTagName("body")[0];
+  
+  // creates a <table> element and a <tbody> element
+  let tbl = document.createElement("table");
+  tbl.setAttribute('id','smallTable');
+  let tblBody = document.createElement("tbody");
+  let element = document.getElementById('smallTable');
+  // creating all cells
+  for (let i = 0; i < 8; i++) {
+    // creates a table row
+    let row = document.createElement("tr");
+    
+    for (let j = 0; j < 3; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      let randomOne = getRandomInt(13);
+      let randomTwo = getRandomInt(13);
+
+      let cell = document.createElement("td");
+
+      if(getRandomInt(2) == 0){
+        cell.innerHTML = (randomOne + " x " + randomTwo + " =    ");
+
+      }
+
+      else{
+        cell.innerHTML = (randomTwo + " x " + randomOne + " =    ");
+
+      }
+      row.appendChild(cell);
+    }
+    
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+  
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tbl);
+  
+  element.parentNode.removeChild(element);
+}
+
 
 
 
 
   // FUNCTION TO GENERATE 5x5 !! FUNCTION TO GENERATE 5x5 !! FUNCTION TO GENERATE 5x5 !! FUNCTION TO GENERATE 5x5 !! FUNCTION TO GENERATE 5x5 !! 
   function generate_table5x5() {
+
+    if ($('#smallTable').length > 0) {
+      let element = document.getElementById('smallTable');
+      element.parentNode.removeChild(element);
+  
+    }
     // get the reference for the body
     let body = document.getElementsByTagName("body")[0];
-  
+    
     // creates a <table> element and a <tbody> element
     let tbl = document.createElement("table");
+    tbl.setAttribute('id','bigTable');
+    let elementTwo = document.getElementById('bigTable');
     let tblBody = document.createElement("tbody");
-  
+    
     // creating all cells
     for (let i = 0; i < 5; i++) {
       // creates a table row
       let row = document.createElement("tr");
-  
+      
       for (let j = 0; j < 5; j++) {
         // Create a <td> element and a text node, make the text
         // node the contents of the <td>, and put the <td> at
         // the end of the table row
+        let randomOne = getRandomInt(13);
+        let randomTwo = getRandomInt(13);
+
         let cell = document.createElement("td");
         cell.className += "fiveByFive";
-        let cellText = document.createTextNode(getRandomInt(13) +  " x " + getRandomInt(13));
-        cell.appendChild(cellText);
+
+
+        // Creating DIV
+        let div = document.createElement("div");
+        let divTwo = document.createElement("div");
+        // Setting both divs to correct text.
+        if(getRandomInt(2) == 0){
+          div.innerHTML = ("    " + randomOne);
+          divTwo.innerHTML = ("x  " + randomTwo);
+  
+        }
+        else{
+          div.innerHTML = ("    " + randomTwo);
+          divTwo.innerHTML = ("x  " + randomOne);
+  
+        }
+        divTwo.setAttribute('id','divTwo');
+        
+        // Placing Divs on screen
+        cell.appendChild(div);
+        div.appendChild(divTwo);
         row.appendChild(cell);
       }
-  
+      
       // add the row to the end of the table body
       tblBody.appendChild(row);
     }
-  
+    
     // put the <tbody> in the <table>
     tbl.appendChild(tblBody);
     // appends <table> into <body>
     body.appendChild(tbl);
 
-  }
+    elementTwo.parentNode.removeChild(elementTwo);
 
+}
